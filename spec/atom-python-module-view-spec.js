14fb21b731ce213e4fa7cb5e4146c3ac1be82d20
Aaron Eikenberry <aeikenberry@gmail.com>
2016-08-13 10:23:12 -0500

    initial commit

---
+++ spec/atom-python-module-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomPythonModuleView from '../lib/atom-python-module-view';
+
+describe('AtomPythonModuleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
